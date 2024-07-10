756d565f087a6972b538e83d972fd111349d6b9c
Nikolay Amiantov <ab@fmap.me>
2024-07-10 19:41:47 +0700

    Fix?

--- module.nix
+++ module.nix
@@ -62,7 +62,7 @@ with lib; let
     };
   };
 
-  maxTimeout = max (mapAttrsToList (proj: proj.timeout) cfg.projects) + 15;
+  maxTimeout = max (mapAttrsToList (name: proj: proj.timeout) cfg.projects) + 15;
 in {
   options = {
     services.deploy-bot = {
